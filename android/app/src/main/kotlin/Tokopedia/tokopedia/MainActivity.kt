993af39cee2ffea02e0e2e0bcabd730d35dcafc0
Fathan_alfarazi <sansfathan@gmail.com>
2023-01-10 12:03:03 +0700

    initial commit

---
+++ android/app/src/main/kotlin/Tokopedia/tokopedia/MainActivity.kt
@@ -0,0 +1,6 @@
+package Tokopedia.tokopedia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/modules/splashScreen/views/splash_screen_view.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:tokopedia/config/warna.dart';
+
+import '../controllers/splash_screen_controller.dart';
+
+class SplashScreenView extends GetView<SplashScreenController> {
+  @override
+  Widget build(BuildContext context) {
+    double tinggi = MediaQuery.of(context).size.height;
+    double lebar = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Container(
+        height: tinggi,
+        padding: EdgeInsets.fromLTRB(20, 50, 30, 20),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [bgLogin, bgLogin2])),
+        child: Column(
+          children: [
+            Container(
+              width: lebar,
+              height: tinggi * 0.3,
+              margin: EdgeInsets.only(bottom: 20),
+              child: Image.asset(
+                "image/sp.png",
+                fit: BoxFit.contain,
+              ),
+            ),
+            Row(
+              children: [
+                Container(
+                  width: 200,
+                  child: Text(
+                    "Let's Get Started",
+                    style: TextStyle(
+                        fontSize: 50,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              "Connect with each other with chatting or calling. Enjoy safe and private texting",
+              style: TextStyle(color: Colors.white, fontSize: 15),
+            ),
+            SizedBox(
+              height: 70,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: 60,
+              child: MaterialButton(
+                  onPressed: () {},
+                  color: Colors.white,
+                  child: Text(
+                    "Join Now",
+                    style: TextStyle(color: bgLogin, fontSize: 20),
+                  )),
+            ),
+            SizedBox(
+              height: 60,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Already have an account?",
+                      style: TextStyle(color: Colors.white, fontSize: 15),
+                    )),
+                TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Login",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ))
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
